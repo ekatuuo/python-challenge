5beaed5c9de50497b441dba5253685e89c6a7245
Edyson Katuuo <ekatuuo@gmail.com>
2021-01-19 18:37:41 -0500

    updated Pyban

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,11 @@
+#Dependencies 
+import csv
+import os
+
+#Files
+file_input = os.path.join("Resources", "budget_data.csv") 
+file_output = os.path.join("Analysis" , "budget_analysis.txt")
+
+with open(file_input) as finance_data:
+    reader = csv.reader(finance_data)
 
